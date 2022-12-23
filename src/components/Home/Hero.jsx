6893b571a46a1e1813c3c6112860c144990ae9b9
Jay Kang <jaykang0395@gmail.com>
2022-12-23 23:18:57 +1300

    feat: added hand wave on hero section enter

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,13 +1,39 @@
 import React from "react";
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
 
 const Hero = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+
   return (
-    <section name="home" className="bg-white dark:bg-neutral-900">
+    <section ref={ref} name="home" className="bg-white dark:bg-neutral-900">
       <div className="grid max-w-screen-xl md:h-screen px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16">
         <div className="flex flex-col items-start mr-auto place-self-center ">
-          <h1 className="max-w-2xl mb-10 text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl dark:text-white">
-            Hello, I'm Jay 👋
-          </h1>
+          <motion.h1
+            className="max-w-2xl mb-10 text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl dark:text-white"
+            animate={{ x: 0, y: 0 }}
+          >
+            Hello, I'm Jay{" "}
+            {isInView ? (
+              <motion.span
+                className="inline-block origin-bottom-right"
+                animate={{ rotate: 20 }}
+                transition={{
+                  repeatType: "reverse",
+                  repeat: 10.5,
+                  from: 0,
+                  duration: 0.6,
+                  ease: "easeInOut",
+                  type: "tween",
+                }}
+              >
+                👋
+              </motion.span>
+            ) : (
+              <span>👋</span>
+            )}
+          </motion.h1>
           <p className="mb-6 font-bold text-gray-500 lg:mb-8 text-2xl md:text-3xl xl:text-4xl dark:text-gray-400">
             I'm a Full-Stack Developer, a sneaker enthusiast, and have passion
             for learning and coding, and I can also fly planes!
